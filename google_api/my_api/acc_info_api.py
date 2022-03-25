41351fb891e3caecfbc2ba00f5c5ff9131ec2b6f
Erik Storjohann <erikstorjohann@Eriks-MacBook-Air.fritz.box>
2022-03-25 15:56:24 +0100

    Google Ads Api. Modified to get inputs from within the code.

--- google_api/my_api/acc_info_api.py
+++ google_api/my_api/acc_info_api.py
@@ -24,6 +24,7 @@ import sys
 from google.ads.googleads.client import GoogleAdsClient
 from google.ads.googleads.errors import GoogleAdsException
 
+customer_id ="5651232347"
 
 def main(client, customer_id):
     ga_service = client.get_service("GoogleAdsService")
@@ -57,24 +58,24 @@ if __name__ == "__main__":
     # home directory if none is specified.
     googleads_client = GoogleAdsClient.load_from_storage("/Users/erikstorjohann/capstone/capstone-project-tem-2/google_api/my_api/googleads.yaml")
 
-    parser = argparse.ArgumentParser(
-        description=(
-            "Displays basic information about the specified "
-            "customer's advertising account."
-        )
-    )
+    # parser = argparse.ArgumentParser(
+    #     description=(
+    #         "Displays basic information about the specified "
+    #         "customer's advertising account."
+    #     )
+    # )
     # The following argument(s) should be provided to run the example.
-    parser.add_argument(
-        "-c",
-        "--customer_id",
-        type=str,
-        required=True,
-        help="The Google Ads customer ID.",
-    )
-    args = parser.parse_args()
+    # parser.add_argument(
+    #     "-c",
+    #     "--customer_id",
+    #     type=str,
+    #     required=True,
+    #     help="The Google Ads customer ID.",
+    # )
+    # args = parser.parse_args()
 
     try:
-        main(googleads_client, args.customer_id)
+        main(googleads_client, customer_id)
     except GoogleAdsException as ex:
         print(
             f'Request with ID "{ex.request_id}" failed with status '
--- google_api/my_api/keyword_plan_api.py
+++ google_api/my_api/keyword_plan_api.py
@@ -26,6 +26,8 @@ import uuid
 from google.ads.googleads.client import GoogleAdsClient
 from google.ads.googleads.errors import GoogleAdsException
 
+#Input the customer_id for the API Call. Only works with deactivated Parser.
+customer_id ="5651232347"
 
 # [START add_keyword_plan]
 def main(client, customer_id):
@@ -286,21 +288,21 @@ if __name__ == "__main__":
     # home directory if none is specified.
     googleads_client = GoogleAdsClient.load_from_storage("/Users/erikstorjohann/capstone/capstone-project-tem-2/google_api/my_api/googleads.yaml")
 
-    parser = argparse.ArgumentParser(
-        description="Creates a keyword plan for specified customer."
-    )
-    # The following argument(s) should be provided to run the example.
-    parser.add_argument(
-        "-c",
-        "--customer_id",
-        type=str,
-        required=True,
-        help="The Google Ads customer ID.",
-    )
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(
+    #     description="Creates a keyword plan for specified customer."
+    # )
+    # # The following argument(s) should be provided to run the example.
+    # parser.add_argument(
+    #     "-c",
+    #     "--customer_id",
+    #     type=str,
+    #     required=True,
+    #     help="The Google Ads customer ID.",
+    # )
+    # args = parser.parse_args()
 
     try:
-        main(googleads_client, args.customer_id)
+        main(googleads_client, customer_id)
     except GoogleAdsException as ex:
         print(
             f'Request with ID "{ex.request_id}" failed with status '
