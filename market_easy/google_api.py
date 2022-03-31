fb7982802e6e346348c6aa6b4f5827f88d00e061
Damjan Oitzl <damjan@iMac-von-Damjan.local>
2022-03-31 13:49:51 +0200

    our package

---
+++ market_easy/google_api.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This example generates keyword ideas from a list of seed keywords."""
+
+
+import argparse
+import sys
+from google.ads.googleads.client import GoogleAdsClient
+from google.ads.googleads.errors import GoogleAdsException
+import pandas as pd
+
+#Input Customer, Location and Language.
+customer_id = "5651232347"
+location_ids = ["2276"]
+language_id = "1001"
+
+#Provide either keyword_texts (as list of strings) or page_url (as string)
+#keyword_texts = ["versicherung", "haftpflichtversicherung", "vollkasko"]
+page_url = ""
+
+#Output = keywords_df
+
+# Location IDs are listed here:
+# https://developers.google.com/google-ads/api/reference/data/geotargets
+# and they can also be retrieved using the GeoTargetConstantService as shown
+# here: https://developers.google.com/google-ads/api/docs/targeting/location-targeting
+_DEFAULT_LOCATION_IDS = ["1023191"]  # location ID for New York, NY
+# A language criterion ID. For example, specify 1000 for English. For more
+# information on determining this value, see the below link:
+# https://developers.google.com/google-ads/api/reference/data/codes-formats#expandable-7
+_DEFAULT_LANGUAGE_ID = "1000"  # language ID for English
+
+
+def main(
+    client, customer_id, location_ids, language_id, keyword_texts, page_url
+):
+    keyword_plan_idea_service = client.get_service("KeywordPlanIdeaService")
+    keyword_competition_level_enum = (
+        client.enums.KeywordPlanCompetitionLevelEnum
+    )
+    keyword_plan_network = (
+        client.enums.KeywordPlanNetworkEnum.GOOGLE_SEARCH_AND_PARTNERS
+    )
+    location_rns = _map_locations_ids_to_resource_names(client, location_ids)
+    language_rn = client.get_service("GoogleAdsService").language_constant_path(
+        language_id
+    )
+
+    # Either keywords or a page_url are required to generate keyword ideas
+    # so this raises an error if neither are provided.
+    if not (keyword_texts or page_url):
+        raise ValueError(
+            "At least one of keywords or page URL is required, "
+            "but neither was specified."
+        )
+
+    # Only one of the fields "url_seed", "keyword_seed", or
+    # "keyword_and_url_seed" can be set on the request, depending on whether
+    # keywords, a page_url or both were passed to this function.
+    request = client.get_type("GenerateKeywordIdeasRequest")
+    request.customer_id = customer_id
+    request.language = language_rn
+    request.geo_target_constants = location_rns
+    request.include_adult_keywords = False
+    request.keyword_plan_network = keyword_plan_network
+
+    # To generate keyword ideas with only a page_url and no keywords we need
+    # to initialize a UrlSeed object with the page_url as the "url" field.
+    if not keyword_texts and page_url:
+        request.url_seed.url = page_url
+
+    # To generate keyword ideas with only a list of keywords and no page_url
+    # we need to initialize a KeywordSeed object and set the "keywords" field
+    # to be a list of StringValue objects.
+    if keyword_texts and not page_url:
+        request.keyword_seed.keywords.extend(keyword_texts)
+
+    # To generate keyword ideas using both a list of keywords and a page_url we
+    # need to initialize a KeywordAndUrlSeed object, setting both the "url" and
+    # "keywords" fields.
+    if keyword_texts and page_url:
+        request.keyword_and_url_seed.url = page_url
+        request.keyword_and_url_seed.keywords.extend(keyword_texts)
+
+    keyword_ideas = keyword_plan_idea_service.generate_keyword_ideas(
+        request=request
+    )
+    keywords_df = []
+    for idea in keyword_ideas:
+        competition_value = idea.keyword_idea_metrics.competition.name
+        keywords_df.append(
+            {       'keyword': idea.text,
+                    'avg_monthly_searches':idea.keyword_idea_metrics.avg_monthly_searches, 
+                    'competition':competition_value, 
+                    #'low_bids':idea.keyword_idea_metrics.low_top_of_page_bid_micros, 
+                    #'high_bids':idea.keyword_idea_metrics.high_top_of_page_bid_micros,
+                    'platform':'google',
+                    'suggested_bid': ((idea.keyword_idea_metrics.low_top_of_page_bid_micros + idea.keyword_idea_metrics.high_top_of_page_bid_micros) / 2)/1000000
+            }
+        )
+    keywords_df = pd.DataFrame(keywords_df)
+    return keywords_df
+
+
+def map_keywords_to_string_values(client, keyword_texts):
+    keyword_protos = []
+    for keyword in keyword_texts:
+        string_val = client.get_type("StringValue")
+        string_val.value = keyword
+        keyword_protos.append(string_val)
+    return keyword_protos
+
+
+def _map_locations_ids_to_resource_names(client, location_ids):
+    """Converts a list of location IDs to resource names.
+
+    Args:
+        client: an initialized GoogleAdsClient instance.
+        location_ids: a list of location ID strings.
+
+    Returns:
+        a list of resource name strings using the given location IDs.
+    """
+    build_resource_name = client.get_service(
+        "GeoTargetConstantService"
+    ).geo_target_constant_path
+    return [build_resource_name(location_id) for location_id in location_ids]
+
+def get_data(keyword_texts):
+    """get data from the google API"""
+    # GoogleAdsClient will read the google-ads.yaml configuration file in the
+    # home directory if none is specified.
+    googleads_client = GoogleAdsClient.load_from_storage("/Users/damjan/neuefische/capstone-project-tem-2/googleads.yaml")
+
+    try:
+        return main(
+            googleads_client,
+            customer_id,
+            location_ids,
+            language_id,
+            keyword_texts,
+            page_url,
+        )
+    except GoogleAdsException as ex:
+        print(
+            f'Request with ID "{ex.request_id}" failed with status '
+            f'"{ex.error.code().name}" and includes the following errors:'
+        )
+        for error in ex.failure.errors:
+            print(f'\tError with message "{error.message}".')
+            if error.location:
+                for field_path_element in error.location.field_path_elements:
+                    print(f"\t\tOn field: {field_path_element.field_name}")
+        sys.exit(1)
