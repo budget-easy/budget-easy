4e707cb72bf1316fd801ef6d2d71bf748ab07762
Damjan Oitzl <98101393+Pagi1@users.noreply.github.com>
2022-04-11 15:46:15 +0200

    Delete market_easy directory

--- market_easy/__init__.py
+++
@@ -1,4 +0,0 @@
-from market_easy.google_api import get_google_data
-from market_easy.bing_api import get_bing_data
-from market_easy.keywords_to_sql import get_sql_data, get_data, to_sql
-from market_easy.keywords_optimization import clicks_max
